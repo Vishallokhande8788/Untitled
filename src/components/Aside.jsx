9907cb23432f70c190acc9d43e3133b1bec08750
vishal lokhande <lvishal108@gmail.com>
2024-12-10 13:42:47 +0000

    layout in home.jsx

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { RiInstagramFill,
+  RiChat3Line,
+  RiMap2Line,
+  RiPhoneLine,
+  RiFacebookBoxLine,
+  RiTwitterXFill,
+  RiLinkedinBoxLine,
+ } from "react-icons/ri";
 
 function Aside() {
-  return <div>Aside</div>;
+  return (<div>
+    <div className="mb-5"> </div>
+  </div>
+  );
 }
 
 export default Aside;
