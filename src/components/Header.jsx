d77841fbecc0f744f581b3ad7a5f0834d538ce75
vishal lokhande <lvishal108@gmail.com>
2024-12-19 14:10:02 +0000

    Conditional show different routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="mb-8 cflex items-center gap-2">
-      <img src="favicon.png" className="w-10" />
+    <header className="mb-8 flex items-center gap-2">
+      <Link to="/">
+        <img src="favicon.png" className="w-10" />
+      </Link>
       <p className="text-lg font-semibold">Untitled UI</p>
     </header>
   );
